7911d3edeaa5b3b72ff330d1bb7663c05d4e8e56
Rain-238 <3164395730@qq.com>
2022-06-28 13:26:04 +0800

    工具类

--- src/main/java/qqrobot/util/LogoFileUtils.java
+++ src/main/java/qqrobot/util/LogoFileUtils.java
@@ -1,12 +1,8 @@
 package qqrobot.util;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.LineIterator;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.security.SecureRandom;
 
 /**
@@ -16,22 +12,19 @@ public class LogoFileUtils {
     public String loadLogo() throws IOException {
         SecureRandom random = new SecureRandom();
         final int number = random.nextInt(18) + 1;
-        File file = new File(String.format("src/main/java/qqrobot/mark/text/logo%s.txt", number));
-        //使用StringBuilder可变字符串
+        InputStream is = this.getClass().getResourceAsStream(String.format("/logo/logo%s.txt", number));
+        //返回读取指定资源的输入流
+        assert is != null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
         StringBuilder result = new StringBuilder();
-        //调用FileUtils的lineIterator行迭代器进行读取
-        try (LineIterator iterator = FileUtils.lineIterator(file, "UTF-8")) {
-            while (iterator.hasNext()) {
-                //一次读取一行
-                String line = iterator.nextLine();
-                //将读取的数据添加到result随后内增加一个行分隔符[System.lineSeparator()]
-                result.append(line).append(System.lineSeparator());
-            }
+        String s;
+        while ((s = br.readLine()) != null) {
+            result.append(s).append(System.lineSeparator());
         }
         return result.toString();
     }
 
-    public static String tips() throws IOException {
+    public String tips() throws IOException {
         final Connection connect = Jsoup.connect("https://v1.hitokoto.cn/?encode=text");
         final String text = connect.timeout(10000 * 1000).ignoreContentType(true).get().select("body").text();
         return String.format("Tips：「%s」", text);
