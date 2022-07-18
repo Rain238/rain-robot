f74f092b4891352fe0c2260db1649f9550ef8ef1
Rain-238 <3164395730@qq.com>
2022-07-18 14:51:47 +0800

    加载Logo

--- src/main/java/qqrobot/mark/LoadMicroMark.java
+++ src/main/java/qqrobot/mark/LoadMicroMark.java
@@ -8,7 +8,7 @@ public class LoadMicroMark {
     public void logo() {
         final LogoFileUtils logo = new LogoFileUtils();
         System.out.print(logo.loadLogo());
-        System.out.println("v1.0.5");
+        System.out.println("v1.0.8");
         System.out.println(logo.tips());
     }
 }
