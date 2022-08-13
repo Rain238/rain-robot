138bb087271c77ca6c4c53472852dcdc355cc31d
Rain-238 <3164395730@qq.com>
2022-08-13 19:26:55 +0800

    数据库结构

--- sql/qqrobot.sql
+++ sql/qqrobot.sql
@@ -11,11 +11,87 @@
  Target Server Version : 80027
  File Encoding         : 65001
 
- Date: 26/06/2022 17:54:17
+ Date: 10/08/2022 17:27:11
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for alicia_all
+-- ----------------------------
+DROP TABLE IF EXISTS `alicia_all`;
+CREATE TABLE `alicia_all`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `U`(`url`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 19349 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for alicia_cat
+-- ----------------------------
+DROP TABLE IF EXISTS `alicia_cat`;
+CREATE TABLE `alicia_cat`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `U`(`url`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2061 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for alicia_mp
+-- ----------------------------
+DROP TABLE IF EXISTS `alicia_mp`;
+CREATE TABLE `alicia_mp`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1829 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for alicia_pc
+-- ----------------------------
+DROP TABLE IF EXISTS `alicia_pc`;
+CREATE TABLE `alicia_pc`  (
+  `id` mediumint NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `U`(`url`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1084 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for alicia_top
+-- ----------------------------
+DROP TABLE IF EXISTS `alicia_top`;
+CREATE TABLE `alicia_top`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `U`(`url`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3397 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for alicia_xing
+-- ----------------------------
+DROP TABLE IF EXISTS `alicia_xing`;
+CREATE TABLE `alicia_xing`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `U`(`url`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 505 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for alicia_yin
+-- ----------------------------
+DROP TABLE IF EXISTS `alicia_yin`;
+CREATE TABLE `alicia_yin`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `U`(`url`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1820 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for bot
@@ -27,7 +103,7 @@ CREATE TABLE `bot`  (
   `bot_qq` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'bot账号',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `QQ`(`friend_qq`) USING BTREE COMMENT '唯一QQ'
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 59 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for genshin
@@ -43,7 +119,7 @@ CREATE TABLE `genshin`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `UID`(`uid`) USING BTREE,
   INDEX `QQ`(`qq`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 34 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 42 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for mihoyo
@@ -60,6 +136,14 @@ CREATE TABLE `mihoyo`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `QQ`(`qq`) USING BTREE,
   UNIQUE INDEX `UID`(`uid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for test
+-- ----------------------------
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test`  (
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
