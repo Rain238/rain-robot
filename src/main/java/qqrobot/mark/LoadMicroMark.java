c0719e162f9148ddf95bc99c57e5dc169a9f059a
Rain-238 <3164395730@qq.com>
2022-08-13 16:28:36 +0800

    加载启动Logo

--- src/main/java/qqrobot/mark/LoadMicroMark.java
+++ src/main/java/qqrobot/mark/LoadMicroMark.java
@@ -1,14 +1,14 @@
 package qqrobot.mark;
 
 import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
 import qqrobot.util.LogoFileUtils;
-
+@Component
 public class LoadMicroMark {
     @SneakyThrows
     public void logo() {
-        final LogoFileUtils logo = new LogoFileUtils();
-        System.out.print(logo.loadLogo());
-        System.out.println("v1.0.8");
-        System.out.println(logo.tips());
+        System.out.print(new LogoFileUtils().loadLogo());
+        System.out.println(new LogoFileUtils().tips());
+        System.out.println("ChestnutDreamBot: v2.0.1");
     }
 }
