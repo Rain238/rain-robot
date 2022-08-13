c1411af459c710a977cf050d8969878b7ac63b83
Rain-238 <3164395730@qq.com>
2022-08-13 16:19:10 +0800

    群聊事件监听类

--- src/main/java/qqrobot/listener/GroupChat.java
+++ src/main/java/qqrobot/listener/GroupChat.java
@@ -19,6 +19,7 @@ import qqrobot.listener.mapper.MihoyoMapping;
 import qqrobot.listener.mapper.ReviseBotMapping;
 import qqrobot.simple.Send;
 
+import java.security.SecureRandom;
 import java.util.*;
 
 /**
@@ -73,24 +74,22 @@ public class GroupChat {
      * 发送消息
      */
     private final Send send;
-    /**
-     * 效验Bot账号
-     */
-    private final BotRegistrar re;
+
     /**
      * 最高权限操控者账号，可在配置文件中修改
      */
     @Value("${master.qq}")
     private String Master;
 
-    public GroupChat(MessageContentBuilderFactory mCBF, BotManager manager, ReviseBotMapping reviseBot, MihoyoMapping mihoyo, GsMapping gs, Send send, BotRegistrar re) {
+    private final Map<String, StringBuilder> qqVerificationCode = new HashMap<>();
+
+    public GroupChat(MessageContentBuilderFactory mCBF, BotManager manager, ReviseBotMapping reviseBot, MihoyoMapping mihoyo, GsMapping gs, Send send) {
         this.mCBF = mCBF;
         this.manager = manager;
         this.reviseBot = reviseBot;
         this.mihoyo = mihoyo;
         this.gs = gs;
         this.send = send;
-        this.re = re;
     }
 
     /**
@@ -100,8 +99,11 @@ public class GroupChat {
      * @param gmi 监听群成员增加事件
      */
     @OnGroupMemberIncrease
-    public synchronized void groupMemberIncrease(@NotNull GroupMemberIncrease gmi, @NotNull Getter getter) {
+    public synchronized void groupMemberIncrease(@NotNull GroupMemberIncrease gmi, @NotNull Getter getter, Setter setter) {
+        log.info("GroupNumber: {} <==-<== NewIncreaseQQ: {} NewIncreaseName: {}", gmi.getGroupInfo().getGroupCode(), gmi.getAccountInfo().getAccountCode(), gmi.getAccountInfo().getAccountNickname());
+        //获取进群者QQ账号
         String qqCode = gmi.getAccountInfo().getAccountCode();
+        //获取本群群号
         String grCode = gmi.getGroupInfo().getGroupCode();
         //获取消息内容生成器
         MessageContentBuilder message = mCBF.getMessageContentBuilder();
@@ -113,31 +115,62 @@ public class GroupChat {
         String eventID = String.format("MGMInc-%s", qqCode);
         //map集合内没有当前事件ID时进入,防止多Bot状态下发送重复消息
         if (!increaseDecreaseMap.containsKey(qqCode)) {
+            //定义取值范围 生成随机的4位数字验证码
+            String str = "0123456789";
+            //容量为4
+            StringBuilder stb = new StringBuilder(4);
+            for (int i = 0; i < 4; i++)
+                stb.append(str.charAt(new SecureRandom().nextInt(str.length())));//遍历4次，拿到某个字符并且拼接
             Timer timer = new Timer();
+            //延迟1.5s后执行 如不延迟有概率发送失败，加群者还没进入就已经执行到了此方法自然@会找不到群成员
             timer.schedule(new TimerTask() {
                 @Override
                 public void run() {
                     send.groups(eventID, grCode, Master, String.format("欢迎%s加入~\n你是本群第%s个成员！", at.getMsg(), total));
                     send.groups(eventID, grCode, Master, "置顶公告中可能会有你想要的各种信息，记得优先查看");
-                    send.groups(eventID, grCode, Master, String.format("%s你需要在接下来的「10分钟」内至少冒泡一次，否则将会被请离喔～", at.getMsg()));
+                    send.groups(eventID, grCode, Master, String.format("%s你需要在「5分钟」内发送验证码%s，否则将会被请离喔～", at.getMsg(), stb));
                 }
             }, 1500);
-            //获取所有Bot
-            List<Bot> bots = manager.getBots();
-            for (int i = 1; i < bots.size(); i++) {
-                final String botCode = bots.get(i).getBotInfo().getAccountCode();
-                BotRegisterInfo r = new BotRegisterInfo(botCode, "");
-                try {
-                    //效验当前Bot账号
-                    re.registerBot(r);
-                    //新入群成员将进行Bot账号绑定
-                    reviseBot.add(qqCode, botCode);
-                    //将当前事件ID添加到map集合内防止多Bot状态下发送重复消息
-                    increaseDecreaseMap.put(qqCode, qqCode);
-                    break;
-                } catch (BotVerifyException e) {
-                    log.error("{}:Bot账号登陆效验失败！", botCode);
+            //将进群者账号和当前生成的随机验证码存入Map集合内
+            qqVerificationCode.put(qqCode, stb);
+            //获取一个默认bot账号
+            String accountCode = manager.getDefaultBot().getSender().getBotInfo().getAccountCode();
+            //新入群成员将进行Bot账号绑定
+            reviseBot.add(qqCode, accountCode);
+            //将当前事件ID添加到map集合内防止多Bot状态下发送重复消息
+            increaseDecreaseMap.put(qqCode, qqCode);
+            //延迟10分钟后执行 如果qqVerificationCode集合内还有该账号记录则未完成验证将其踢除
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    if (qqVerificationCode.containsKey(qqCode)) {
+                        setter.setGroupMemberKick(grCode, qqCode, "未在规定时间内完成验证~", false);
+                    }
                 }
+            }, 5 * 60 * 1000);
+        }
+    }
+
+    /**
+     * 获取验证码
+     * @param msg GroupMsg 群聊消息
+     */
+    @OnGroup
+    @Filter(value = "^[0-9]{4}", matchType = MatchType.REGEX_MATCHES)
+    public void getVerificationCode(GroupMsg msg) {
+        //获取当前QQ账号
+        String qqCode = msg.getAccountInfo().getAccountCode();
+        //根据当前QQ账号查找qqVerificationCode集合内是否存在记录
+        if (qqVerificationCode.containsKey(qqCode)) {
+            //获取消息体
+            String text = msg.getText();
+            //获取当前集合内的验证码
+            StringBuilder stb = qqVerificationCode.get(qqCode);
+            //判断消息体是否与验证码一致，一致则为验证成功
+            if (text.equals(stb.toString())) {
+                MessageContent at = mCBF.getMessageContentBuilder().at(qqCode).build();
+                qqVerificationCode.remove(qqCode);
+                send.groups(msg, String.format("%s已通过验证", at.getMsg()));
             }
         }
     }
@@ -150,6 +183,7 @@ public class GroupChat {
      */
     @OnGroupMemberReduce
     public synchronized void getAccountInfo(@NotNull GroupMemberReduce gbr) {
+        log.info("ExitReminder: {}已从{}群中离开...", gbr.getAccountInfo().getAccountCode(), gbr.getGroupInfo().getGroupCode());
         String grCode = gbr.getGroupInfo().getGroupCode();
         String qqCode = gbr.getAccountInfo().getAccountCode();
         String accountName = gbr.getAccountInfo().getAccountNickname();
@@ -167,6 +201,8 @@ public class GroupChat {
                 reviseBot.deleteAccordingToQQ(qqCode);
                 //将该账号在Map内删除
                 increaseDecreaseMap.remove(qqCode);
+
+                qqVerificationCode.remove(qqCode);
             }
         } catch (Exception e) {
             log.warn("{}已退群...", accountName);
@@ -176,15 +212,14 @@ public class GroupChat {
     /**
      * 监听群添加请求事件 自动同意入群
      *
-     * @param groupAddRequest 群添加请求事件
+     * @param request 群添加请求事件
      */
     @OnGroupAddRequest
-    public void groupAddRequest(@NotNull GroupAddRequest groupAddRequest, @NotNull Setter setter) {
+    public void groupAddRequest(@NotNull GroupAddRequest request, @NotNull Setter setter) {
+        log.info("EnterGroupReminder: {}请求进入{}群,入群理由: {}", request.getAccountInfo().getAccountCode(), request.getGroupInfo().getGroupCode(), request.getFlag());
         //自动同意入群申请                        简介                     是否同意       是否黑名单    理由
-        setter.setGroupAddRequestAsync(groupAddRequest.getFlag(), true, false, "");
+        setter.setGroupAddRequestAsync(request.getFlag(), true, false, "");
     }
-
-
 
 
     /**
@@ -195,6 +230,7 @@ public class GroupChat {
     @OnGroup
     @Filter(value = "栗梦", matchType = MatchType.EQUALS, trim = true)
     public synchronized void botList(@NotNull GroupMsg msg) {
+        log.info("TriggerQQ: {} TriggerWord: {}", msg.getAccountInfo().getAccountCode(), msg.getText());
         if (!map.containsKey(msg.getId())) {
             String qqCode = msg.getAccountInfo().getAccountCode();
             String grCode = msg.getGroupInfo().getGroupCode();
@@ -209,7 +245,7 @@ public class GroupChat {
                 String botQQCode = bot.getBotInfo().getAccountCode();
                 //指定一个Bot账号进行发送消息
                 Bot bot1 = manager.getBot(botQQCode);
-                bot1.getSender().SENDER.sendGroupMsg(grCode, String.format("%s\n我在", at));
+                bot1.getSender().SENDER.sendGroupMsg(grCode, String.format("%s\n干嘛啦", at));
             }
             map.put(msg.getId(), msg.getId());
         }
