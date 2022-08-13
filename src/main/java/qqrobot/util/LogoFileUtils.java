2877af40242054fc4cba1ea8f1df783f06c6a491
Rain-238 <3164395730@qq.com>
2022-08-13 17:23:15 +0800

    读取Logo文件工具

--- src/main/java/qqrobot/util/LogoFileUtils.java
+++ src/main/java/qqrobot/util/LogoFileUtils.java
@@ -3,16 +3,21 @@ package qqrobot.util;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.SecureRandom;
 
 /**
  * 加载微标Logo
+ * &#064;Author  RainRain
+ * &#064;Data  2022/7/20 12:02
  */
 public class LogoFileUtils {
     public String loadLogo() throws IOException {
         SecureRandom random = new SecureRandom();
-        final int number = random.nextInt(11) + 1;
+        final int number = random.nextInt(10) + 1;
         InputStream is = this.getClass().getResourceAsStream(String.format("/logo/logo%s.txt", number));
         //返回读取指定资源的输入流
         assert is != null;
